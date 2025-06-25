e39aaf1ae0a3d6b02ff6a3cdd72b02a5080b0738
dat041105 <162545570+dat147953@users.noreply.github.com>
2025-06-25 09:00:59 +0700

    Add files via upload

---
+++ DNUStudyGroups/DNUStudyGroups/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DNUStudyGroups.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
